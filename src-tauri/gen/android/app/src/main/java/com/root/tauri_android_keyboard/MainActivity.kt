821dfa10b8ba4e4c01888178ca7062b79905b3ba
bestfood-lxj <lixj@321tips.com>
2025-11-27 12:07:29 +0800

    :beers: Write code drunkenly.

--- src-tauri/gen/android/app/src/main/java/com/root/tauri_android_keyboard/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/root/tauri_android_keyboard/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    showSoftKeyboard()
+  }
+  fun showSoftKeyboard(view: View) {
+   if (view.requestFocus()) {
+       val imm = getSystemService(InputMethodManager::class.java)
+       imm.showSoftInput(this, InputMethodManager.SHOW_FORCED)
+   }
   }
 }
